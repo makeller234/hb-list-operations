b34d3a37e2bfa76a249aff50ac743ea212b08eb0
Melissa Keller <melissa.keller23@gmail.com>
2021-09-30 16:03:25 -0500

    iitial commit, plus solved everything

---
+++ further_study.py
@@ -0,0 +1,327 @@
+"""Custom implementations of several standard Python list methods."""
+
+from list_operations import *
+
+
+def custom_len(input_list):
+    """Return number of items in the list.
+
+    The function custom_len(input_list) should have
+    the same functionality and result as len(input_list).
+
+    For example:
+
+        >>> custom_len(['Do', 'Re', 'Mi', 'Fa', 'So', 'La', 'Ti', 'Do'])
+        8
+    
+
+    """
+    length = 0
+    for items in input_list:
+        length += 1
+
+
+    return length
+
+
+# For the next four exercises, you'll need to be clever and think about ways
+# to use list slice assignment.
+#
+# NOTE: these are especially contrived. You wouldn't really want
+# to typically append things to a list like this (you'd want to to use the
+# list.append() method), but we want you to practice list slicing assignment
+# in different ways so it sticks in your brain.
+
+
+def custom_append(input_list, value):
+    """Add the value to the end of the list.
+
+    The function custom_append(input_list, value) should have the same
+    functionality as input_list.append(value) where value is added to the
+    end of the list and the function returns nothing.
+
+    For example:
+
+        >>> notes = ['Do', 'Re', 'Mi', 'Fa', 'So', 'La', 'Ti', 'Do']
+        >>> custom_append(notes, 'Re')
+        >>> notes == ['Do', 'Re', 'Mi', 'Fa', 'So', 'La', 'Ti', 'Do', 'Re']
+        True
+
+    """
+    size = len(input_list)
+    input_list[size:] = [value]
+
+    pass
+
+
+def custom_extend(input_list, second_list):
+    """Append every item in second_list to input_list.
+
+    Like input_list.extend(second_list), custom_extend(input_list, second_list)
+    should append every item in the second list to the end of the first list
+    and return nothing.
+
+    For example:
+
+        >>> months = ['Jan', 'Feb', 'Mar']
+        >>> custom_extend(months, ['Apr', 'May'])
+        >>> months == ['Jan', 'Feb', 'Mar', 'Apr', 'May']
+        True
+
+    """
+    size = len(input_list)
+    input_list[size:] = second_list
+
+    pass
+
+
+def custom_insert(input_list, index, value):
+    """Insert value at index in the list.
+
+    Like input_list.insert(index, value), should insert (not replace) the value
+    at the specified index of the input list and return nothing.
+
+    For example:
+
+        >>> months = ['Jan', 'Mar']
+        >>> custom_insert(months, 1, 'Feb')
+        >>> months == ['Jan', 'Feb', 'Mar']
+        True
+
+    """
+    input_list[index:index] = [value]
+
+    pass
+
+
+def custom_remove(input_list, value):
+    """Remove the first item of the value in list.
+
+    The function custom_remove(input_list, value) should have the same
+    functionality as input_list.remove(value) where the first item of
+    the value specified is removed and the function returns nothing.
+
+    For example:
+
+        >>> notes = ['Do', 'Re', 'Mi', 'Fa', 'So', 'La', 'Ti', 'Do']
+        >>> custom_remove(notes, 'Do')
+        >>> notes == ['Re', 'Mi', 'Fa', 'So', 'La', 'Ti', 'Do']
+        True
+
+    """
+    i = 0
+    for item in input_list:
+        if item == value:
+            input_list.pop(i)
+            break
+        else:
+            i+=1
+
+
+    pass
+
+
+def custom_pop(input_list):
+    """Remove the last item in the list and returns it.
+
+    The function custom_pop(input_list) should have the same functionality
+    and result as input_list.pop().
+
+    For example:
+
+        >>> months = ['Jan', 'Feb', 'March']
+        >>> custom_pop(months)
+        'March'
+        >>> months
+        ['Jan', 'Feb']
+
+    """
+    # print(input_list[-1])
+    # del input_list[-1]
+
+    #print(input_list[-1])
+    # print(input_list)
+    # new_list = []
+    # # for item in input_list:
+    # #     new_list.append(item)
+
+    # print(str(input_list[-1]))
+    # i = 0
+    # while i < len(input_list):
+    #     new_list.append(input_list[i])
+    #     i += 1
+
+    last_item = input_list[-1]
+    del input_list[-1]
+    return last_item
+
+    #return None
+
+
+    #return input_list[:-2]
+
+
+def custom_index(input_list, value):
+    """Return the index of the first item of value found in input_list.
+
+    The function custom_index(input_list, value) should have the same
+    functionality and result as input_list.index(value).
+
+    For example:
+
+        >>> custom_index(['Do', 'Re', 'Mi', 'Fa', 'So', 'La', 'Ti', 'Do'], 'Re')
+        1
+
+    """
+    i = 0
+    for item in input_list:
+        if item == value:
+            return i
+        else:
+            i += 1
+
+    #return 0
+
+
+def custom_count(input_list, value):
+    """Return the number of times value appears in the list.
+
+    Like input_list.count(value), custom_count(input_list, value) should
+    return the number of times the specified value appears in the list.
+
+    For example:
+
+        >>> custom_count(['Do', 'Re', 'Mi', 'Fa', 'So', 'La', 'Ti', 'Do'], 'Do')
+        2
+
+    """
+    count = 0
+    for item in input_list:
+        if item == value:
+            count +=1
+
+    return count
+
+
+def custom_reverse(input_list):
+    """Reverse the elements of the input_list.
+
+    Like input_list.reverse(), custom_reverse(input_list) should reverse the
+    elements of the original list and return nothing (we call this reversing
+    "in place").
+
+    For example:
+
+        >>> multiples = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
+        >>> custom_reverse(multiples)
+        >>> multiples == [27, 24, 21, 18, 15, 12, 9, 6, 3, 0]
+        True
+
+    """
+
+    end_index = len(input_list)-1
+    index_rev = 0
+    update_while = len(input_list) -1
+
+
+    while update_while >= 0:
+        input_list[index_rev:index_rev] = [input_list[end_index]]
+        del input_list[-1]
+        update_while -= 1
+        index_rev += 1
+
+
+    pass
+
+
+def custom_contains(input_list, value):
+    """Return True or False if value is in the input_list.
+
+    Like (value in input_list), should return True if the list contains the
+    specified value and False if it does not. Remember, do not use the `if X in Y`
+    statement -- find another way to solve it!
+
+    For example:
+
+        >>> custom_contains([0, 3, 6, 9, 12, 15, 18, 21, 24], 23)
+        False
+
+        >>> custom_contains([0, 3, 6, 9, 12, 15, 18, 21, 24], 24)
+        True
+
+    """
+    # temp_var = 0
+    # item_index = 0
+    # for item in input_list:
+    #     if item[item_index] == value:
+    #         temp_var += 1
+    #         item_index += 1
+    #         print(f" if {temp_var}")
+    #     else:
+    #         temp_var = temp_var
+    #         print(f" else {temp_var}")
+        
+    #     if temp_var > 0:
+    #         return True
+    #     else:
+    #         return False
+
+    for item in input_list:
+        if item == value:
+            return True
+        else:
+            continue
+    return False
+
+
+def custom_equality(some_list, another_list):
+    """Return True if passed lists are identical, False otherwise.
+
+    Like (some_list == another_list), custom_equality(some_list, another_list)
+    should return True if both lists contain the same values in the same indexes.
+
+    For example:
+
+        >>> custom_equality(['Jan', 'Feb', 'Mar'], ['Jan', 'Feb', 'Mar'])
+        True
+
+        >>> custom_equality(['Jan', 'Feb', 'Mar'], ['Jan', 'Mar', 'Feb'])
+        False
+
+    """
+
+    # matching_items = 0
+    # for item in some_list:
+    #     for item2 in another_list:
+    #         if item == item2:
+    #             #matching_items += 1
+    #             return True
+    #         else:
+    #             continue
+    # return False
+
+    i = 0
+    count = 0
+    for item in some_list:
+        if item == another_list[i]:
+            count = count + 1
+            i = i + 1
+        else:
+            count = count
+            i = i + 1
+    
+    if count == len(some_list):
+        return True
+    else:
+        return False
+
+
+
+# This is the part were we actually run the doctests.
+
+if __name__ == "__main__":
+    import doctest
+
+    result = doctest.testmod()
+    if result.failed == 0:
+        print("ALL TESTS PASSED")
